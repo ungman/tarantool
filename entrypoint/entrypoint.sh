f7184fdc4aca48b3b9471143c990b6c3df1e2ccd
myungman <myungman@phoenixit.ru>
2024-01-23 23:27:29 +0300

    add run docker to cluster

---
+++ entrypoint/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+cd /app
+echo "Building application"
+cartridge build
+echo "Start cartridge cluster in background"
+cartridge start --run-dir=/tmp/run --data-dir=/tmp/data -d
+sleep 10
+echo "Setup topology"
+cartridge replicasets setup --bootstrap-vshard --run-dir /tmp/run
+sleep 10
+echo "Run migrations"
+curl -X POST http://localhost:8081/migrations/up
+
+#echo "Loading data"
+#tarantool /entrypoint/load_data.lua
+
+echo "Restarting cluster"
+cartridge stop --run-dir=/tmp/run
+sleep 10
+cartridge start --run-dir=/tmp/run --data-dir=/tmp/data
---
+++ entrypoint/test_data.lua
@@ -0,0 +1,14 @@
+crud.insert("routerSessionContexts", { 'sessionId',
+                                       { field1 = 'Data field 1', field2 = 'Data field 2' },
+                                       1
+})
+
+crud.insert("routerSessionContexts", { 'sessionId_2',
+                                       { field1 = 'Data field 1', field2 = 'Data field 2' },
+                                       2
+})
+
+crud.insert("routerSessionContexts", { 'sessionId_3',
+                                       { field1 = 'Data field 1', field2 = 'Data field 2' },
+                                       3
+})
